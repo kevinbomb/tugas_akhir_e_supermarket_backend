f07ec48097ac6842f41951c94a4afea517269a72
melatisukma1666 <melatisukma083@gmail.com>
2022-12-20 18:47:50 +0700

    EmailVeriffied

--- resources/views/emailSuccess.blade.php
+++ resources/views/emailSuccess.blade.php
@@ -1,3 +1,9 @@
-<h1>
-    Berhasil verifikasi email
-</h1>
+<html>
+    <img src="https://i.pinimg.com/originals/1f/74/bd/1f74bde322db3c3034fcae205c10cd00.png" style="width: 100%; height: 100%" />
+    <div class="text-left" style="color: #fff; position: absolute; top: 40%; left: 28%; right: 20%">
+    <h1 style="color: black;">Email verified successfully!</h1>
+    <h4 style="color: pink;">
+        <i>By Minnie Market</i>
+    </h4>
+    </div>
+</html>
